8f2244d486391d5dde988574df32883c3f31bf5d
Anca Rus <anca.rus30@yahoo.com>
2025-10-14 13:39:16 +0300

    refactor: simplify routing in App component and add navbar component

--- src/App.js
+++ src/App.js
@@ -8,9 +8,8 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/' element={<h1>Home Page</h1>}/>
-          <Route path='/about' element={<h1>About Page</h1>}/>
-          <Route path='/contact' element={<h1>Contact Page</h1>}/>
+          <Route path='/' />
+          <Route path='/cart' />
         </Routes>
       </Router>
       
