1f8b85a02924dba4adf8edef8d47310baf537b58
Anca Rus <anca.rus30@yahoo.com>
2025-10-18 10:46:14 +0300

    Add Cart and Shop components with product listing
    - Created Cart component as a placeholder for the shopping cart functionality.
    - Developed Product component to display individual product details including image, name, and price.
    - Implemented Shop component to render a list of products using the Product component.
    - Added PRODUCTS array in products.js to store product data including images, names, and prices.

---
+++ src/products.js
@@ -0,0 +1,61 @@
+import product1 from './assetss/1.png';
+import product2 from './assetss/2.png';
+import product3 from './assetss/3.png';
+import product4 from './assetss/4.png';
+import product5 from './assetss/5.png';
+import product6 from './assetss/6.png';
+import product7 from './assetss/7.png';
+import product8 from './assetss/8.png';
+
+
+export const PRODUCTS = [
+{
+    id:1,
+    productName: "IPhone",
+    price: 999.99,
+    productImage: product1,
+},
+{
+    id:2,
+    productName: "Macbook Pro 2022",
+    price: 1999.99,
+    productImage: product2,
+},
+{
+    id:3,
+    productName: "Cannon M50 Camera",
+    price: 699.99,
+    productImage: product3,
+},
+{
+    id:4,
+    productName: "WLS Van Gogh Denim Jacket",
+    price: 228.99,
+    productImage: product4,
+},
+{
+    id:5,
+    productName: "LED Light Strips",
+    price: 19.99,
+    productImage: product5,
+},
+{
+    id:6,
+    productName: "SPECTRUM LST TEE",
+    price: 68.99,
+    productImage: product6,
+},
+
+{
+    id:7,
+    productName: "AUTO SERVICE SHIRT by GolfWang",
+    price: 120.99,
+    productImage: product7,
+},
+{
+    id:8,
+    productName: "DONT TRIP HAT",
+    price: 40.99,
+    productImage: product8,
+},
+]
